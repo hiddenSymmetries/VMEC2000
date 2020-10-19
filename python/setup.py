edb680b74c71ce71dca0880ea4883dacef58104b
Bharat Kumar Medasani <mbkumar@gmail.com>
2020-10-19 12:25:39 -0400

    Bug fix to copy python modules to install folder

--- python/setup.py
+++ python/setup.py
@@ -2,14 +2,18 @@
 import sys
 import json
 import argparse
-
 import setuptools
+
+from os.path import basename, splitext
+from glob import glob
+
 print("system.platform is {}".format(sys.platform))
 if (sys.platform == "darwin"):
     from distutils import sysconfig
     vars = sysconfig.get_config_vars()
     vars['LDSHARED'] = vars['LDSHARED'].replace('-bundle', '-dynamiclib')
 
+from setuptools import find_packages
 from skbuild import setup
 
 #parser = argparse.ArgumentParser(
@@ -26,8 +30,19 @@ with open('cmake_config_file.json') as fp:
 setup(
     name="vmec",
     version="0.0.1",
-    packages=["vmec"],
+    license="MIT",
+    #packages=["vmec"],
+    packages=['vmec'],
+    package_dir={'': 'src'},
+    #py_modules=[splitext(basename(path))[0] for path in glob('src/vmec/*.py')],
     install_requires=['f90wrap == v0.2.3'],
+    classifiers=[
+        "Development Status :: 1 - Alpha",
+        "Intended Audience :: Nuclear Fusion Community",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: MHD Equilibrium Solver"],
     cmake_args=d['cmake_args'],
            # [
            #'-DCMAKE_C_COMPILER=mpicc',
